7e93fe9f914a029f221e6a8b9e9c9c2a9c74ff13
yoinqui <yoann.inquimbert@viacesi.fr>
2026-01-10 00:14:17 +0100

    init-test

---
+++ keiko-hub/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'KeikoHub',
+  webDir: 'www'
+};
+
+export default config;
